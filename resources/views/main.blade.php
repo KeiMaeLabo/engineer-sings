39850036ef21e17bb4a10e26f796cbc5d8bc80d9
KeiMaeLabo <65949665+KeiMaeLabo@users.noreply.github.com>
2021-06-20 16:41:09 +0900

    Song モーダル追加

--- resources/views/main.blade.php
+++ resources/views/main.blade.php
@@ -10,6 +10,7 @@
         <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@200;600&display=swap" rel="stylesheet">
 
         <!-- Styles -->
+        <link href="https://use.fontawesome.com/releases/v5.6.1/css/all.css" rel="stylesheet">
         <link href="{{ asset('css/app.css') }}" rel="stylesheet">
         <link href="{{ asset('css/main.css') }}" rel="stylesheet">
         <style>
@@ -19,16 +20,41 @@
     <body>
         <div class="content" id="app">
             <div class="left-content">
+                <label for="input-video" class="buttons"><i class="fas fa-video"></i></label>
+                <input id="input-video" type="file" accept="video/*" v-on:change="handleFileSelect">
+                <button type="button" id="lyric" class="buttons" data-toggle="modal" data-target="#lyricModal"><i class="fas fa-edit"></i></button>
             </div>
             <div class="main-content">
-                <label for="input-video">動画を選択</label>
-                <input id="input-video" type="file" accept="video/*" v-on:change="handleFileSelect">
                 <video controls v-if="src" muted>
                     <source :src="src" type='video/mp4; codecs="avc1.64001E,mp4a.40.2"'>
                 </video>
             </div>
             <div class="right-content">
             </div>
+        </div>
+        <!-- Modal -->
+        <div class="modal fade" id="lyricModal" tabindex="-1" role="dialog" aria-labelledby="lyricModalTitle" aria-hidden="true">
+        <div class="modal-dialog modal-dialog-centered" role="document">
+            <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title" id="lyricModalTitle">Song</h5>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                <span aria-hidden="true">&times;</span>
+            </div>
+            <div class="modal-body">
+                <label for="title" class="col-1">title</label><input type="text" id="title" class="col-5">
+                <div class="divide">
+                    <label for="lyrics" class="col-1">lyrics <i class="fas fa-user"></i></label><input type="text" id="lyrics" class="col-3">
+                    <label for="song" class="col-1">song <i class="fas fa-user"></i></label><input type="text" id="song" class="col-3">
+                </div>
+                <label for="playtime" class="col-1">playtime</label><input type="text" id="playtime"class="col-1">
+                <textarea class="col-12" style="height:40vh"></textarea>
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-primary">save <i class="far fa-save"></i></button>
+            </div>
+            </div>
+        </div>
         </div>
     </body>
     <script>
