5a9e344c2482d670550a0f3f22fcfb964c317097
KeiMaeLabo <65949665+KeiMaeLabo@users.noreply.github.com>
2021-07-04 15:30:18 +0900

    modified css, add song table migration

--- database/migrations/2021_07_04_044321_create_songs_table.php
+++ database/migrations/2021_07_04_044321_create_songs_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreatePasswordResetsTable extends Migration
+class CreateSongsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,10 +13,13 @@ class CreatePasswordResetsTable extends Migration
      */
     public function up()
     {
-        Schema::create('password_resets', function (Blueprint $table) {
-            $table->string('email')->index();
-            $table->string('token');
-            $table->timestamp('created_at')->nullable();
+        Schema::create('songs', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('title');
+            $table->string('artist');
+            $table->string('playtime');
+            $table->string('lyric');
+            $table->timestamps();
         });
     }
 
@@ -27,6 +30,6 @@ class CreatePasswordResetsTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('password_resets');
+        Schema::dropIfExists('songs');
     }
 }
--- resources/views/main.blade.php
+++ resources/views/main.blade.php
@@ -11,17 +11,17 @@
 
         <!-- Styles -->
         <link href="https://use.fontawesome.com/releases/v5.6.1/css/all.css" rel="stylesheet">
-        <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+        <link href="{{ asset('css/bootstrap.min.css') }}" rel="stylesheet">
         <link href="{{ asset('css/main.css') }}" rel="stylesheet">
         <style>
 
         </style>
     </head>
     <body>
-        <div class="content" id="app">
+        <div class="container-fluid d-flex p-0" id="app">
             <div class="left-content">
                 <label for="input-video" class="buttons"><i class="fas fa-video"></i></label>
-                <input id="input-video" type="file" accept="video/*" v-on:change="handleFileSelect">
+                <input id="input-video" class="d-none" type="file" accept="video/*" v-on:change="handleFileSelect">
                 <button type="button" id="lyric" class="buttons" data-toggle="modal" data-target="#lyricModal"><i class="fas fa-edit"></i></button>
             </div>
             <div class="main-content">
@@ -42,16 +42,26 @@
                 <span aria-hidden="true">&times;</span>
             </div>
             <div class="modal-body">
-                <label for="title" class="col-1">title</label><input type="text" id="title" class="col-5">
-                <div class="divide">
-                    <label for="lyrics" class="col-1">lyrics <i class="fas fa-user"></i></label><input type="text" id="lyrics" class="col-3">
-                    <label for="song" class="col-1">song <i class="fas fa-user"></i></label><input type="text" id="song" class="col-3">
+                <div class="row">
+                    <label for="title" class="form-label col-1">title</label>
+                    <input type="text" id="title" class="form-control form-control-sm col-5">
+                    <button type="button" class="btn btn-success btn-sm ml-5">Get lyric <i class="far fa-file-alt"></i></button>
                 </div>
-                <label for="playtime" class="col-1">playtime</label><input type="text" id="playtime"class="col-1">
-                <textarea class="col-12" style="height:50vh"></textarea>
+                <div class="row">
+                    <label for="lyrics" class="form-label col-1">artist <i class="fas fa-user"></i></label>
+                    <input type="text" id="lyrics" class="form-control form-control-sm col-5">
+                </div>
+                <div class="row">
+                    <label for="playtime" class="form-label col-1">playtime</label>
+                    <input type="text" id="playtime" class="form-control form-control-sm col-1" placeholder="00:00">
+                </div>
+                <div class="row">
+                    <label class="form-label col-1">lyric</label>
+                    <textarea class="form-control form-control-sm col-10" style="height:55vh"></textarea>
+                </div>
             </div>
-            <div class="modal-footer">
-                <button type="button" class="btn btn-primary">save <i class="far fa-save"></i></button>
+            <div class="modal-footer justify-content-center">
+                <button type="button" class="btn btn-primary btn-lg">save <i class="far fa-save"></i></button>
             </div>
             </div>
         </div>
@@ -65,13 +75,10 @@
             },
             methods: {
                 handleFileSelect(event) {
-                    // reset data
                     this.src = null;
 
-                    // validate file
                     const file = event.target.files[0];
                     if (file.type.match('video/*')) {
-                        // read file
                         const fileReader = new FileReader();
                         fileReader.onload = (evt) => {
                             this.src = evt.target.result;
